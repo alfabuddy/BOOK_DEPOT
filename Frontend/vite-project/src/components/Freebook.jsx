fac740082c9d9e46f1a1eb8520b284c8b88ccc25
alfabuddy <abhishek9852815692@gmail.com>
2025-02-21 21:21:27 +0530

    Update Freebook.jsx

--- Frontend/vite-project/src/components/Freebook.jsx
+++ Frontend/vite-project/src/components/Freebook.jsx
@@ -12,7 +12,7 @@ function Freebook() {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("http://localhost:4001/book");
+        const res = await axios.get("https://book-depot-5.onrender.com/book");
         // console.log(res.data);
 
         // ✅ Correctly filtering books
